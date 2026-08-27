0d4174698dfbb1837d4deabefcc0c099355ffb7a
kiranvajja16 <vajjakiran2005@gmail.com>
2026-08-27 16:38:27 +0530

    mongodb to mercchant dashboard is connedted

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ app.get("/", (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
   await connectDB();
